5bc53743157080344c5ceed746b92b10273b520a
GioCharro <georgiocharro@hotmail.com>
2024-12-18 20:03:03 +0200

    [Web] Fixed miner text change

--- src/components/homes/home-1/BlogAreaHomeOne.tsx
+++ src/components/homes/home-1/BlogAreaHomeOne.tsx
@@ -9,7 +9,7 @@ const BlogAreaHomeOne = () => {
               <div className="col-lg-10 offset-lg-1 col-sm-12 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s" data-wow-offset="0">
                 <div className="subs_form">
                   <h3>Subscribe to our Blogs where you will receive a notfication on your email</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elitsed eiusmod tempor enim minim</p>
+                  <p>Stay updated with the latest Blogs. We promise — no spam, just the good stuff... like a perfectly baked pizza in your inbox!</p>
                   <form onClick={(e) => e.preventDefault()} className="home_subs">
                     <input type="text" className="subscribe__input" placeholder="Enter your Email Address" />
                     <button type="button" className="subscribe__btn"><i className="ti-new-window"></i></button>
