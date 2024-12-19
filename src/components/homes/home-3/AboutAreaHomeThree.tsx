5ba7f68bbc244911f65945107306032ef68d2075
GioCharro <georgiocharro@hotmail.com>
2024-12-19 17:09:33 +0200

    [Web] Text change

--- src/components/homes/home-3/AboutAreaHomeThree.tsx
+++ src/components/homes/home-3/AboutAreaHomeThree.tsx
@@ -33,7 +33,7 @@ const AboutAreaHomeThree = () => {
           <div className="col-lg-6">
             <div className="ab_content">
               <p className="text-lg">
-              At CyberMorph Technologies, our mission is clear and transformative: to redefine how businesses protect their critical assets and embrace the potential of AI-driven innovation. By integrating advanced cybersecurity strategies with cutting-edge artificial intelligence, we empower organizations to strengthen their resilience, adapt to dynamic challenges, and seize new opportunities in today’s fast-paced digital world.
+              At CyberMorph Technologies, our mission is clear and transformative, to redefine how businesses protect their critical assets and embrace the potential of AI-driven innovation, by integrating advanced cybersecurity strategies with cutting-edge artificial intelligence. We empower organizations to strengthen their resilience, adapt to dynamic challenges, and seize new opportunities in today’s fast-paced digital world.
               </p>
               <ul className="list-unstyled">
                 <li className="mb-3 mt-3">
